ac99edf8bc86a3b494d39b67bb8aa013bfaaef72
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-01 01:27:34 +0000

    Snapshot of branch master taken on 2019-04-30T21:27:33.041-04:00 from "https://github.com/UnofficialJuliaMirror/JuliaGraphics-FreeTypeBuilder"

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -62,6 +62,7 @@ platforms = [
     Linux(:x86_64, libc=:glibc),
     Linux(:aarch64, libc=:glibc),
     Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
     Linux(:i686, libc=:musl),
     Linux(:x86_64, libc=:musl),
     Linux(:aarch64, libc=:musl),
